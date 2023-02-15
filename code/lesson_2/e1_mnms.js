094dff8afb4ed505d4025243011e8d5c5cbb9802
Jan Krutisch <jan@krutisch.de>
2023-02-15 17:25:32 +0100

    prep lesson 2

---
+++ code/lesson_2/e1_mnms.js
@@ -0,0 +1,14 @@
+// Birthday party. We have x M&Ms
+// we have y Kids in attendence.
+// The sweets should be divided up evenly
+// How many are remaining so that dad or mom
+// can snack on them?
+
+// Input: number of M&Ms, number of Kids
+// Output should look like this:
+// Daddy gets 12 M&M's
+
+let kids = 7
+let sweets = 60
+
+// ??
---
+++ code/lesson_2/index.md
@@ -0,0 +1,14 @@
+# Lesson 2
+
+## Checkin
+
+## Homework / Repetition / Jargon
+
+- Statements and Expressions
+- Variables and Constants
+- How the computer inteprets a program
+
+## NodeJS
+
+- Installation
+- Integration in VSCode
