e17a970635f2a7d052a107b65ab75d935e43781e
Jan Krutisch <jan@krutisch.de>
2023-02-27 18:09:54 +0100

    Unsaved changes

---
+++ code/lesson_5/array_1.js
@@ -0,0 +1,8 @@
+let name = "Jan";
+let age = 67;
+let hasPet = false;
+let nix = null;
+
+let names = ["Jan", "Sandra"];
+
+names[0];
--- code/lesson_5/for.js
+++ code/lesson_5/for.js
@@ -1,8 +1,17 @@
 let counter = 0;
 while (counter < 20) {
+  console.log(counter);
   counter++;
 }
 
-for (let counter = 0; counter < 20; counter++) {
+for (let counter = 1; counter <= 20; counter++) {
   console.log(counter);
+}
+
+for (let line = 1; line <= 20; line++) {
+  console.log(line);
+}
+
+for (let i = 0; i < 20; i++) {
+  console.log(i);
 }
